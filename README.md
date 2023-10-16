860017f1180929fdafdaf2b43bbbe770023ac87a
Sam-programs <None>
2023-10-16 13:53:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,122 @@
+# expand.nvim
+a plugin that allows u to quickly expand statements
+# demo 
+```
+| is the cursor
+in c 
+if (true|)  C-Space
+
+if (true){
+    |
+}
+
+typedef struct foo |
+
+typedef struct foo {
+    |
+};
+
+in lua
+if something| C-Space
+
+if something then
+|
+end
+
+local myvar = | C-space
+
+local myvar = {
+    |
+}
+```
+## installation
+packer
+```lua
+use {
+    "Sam-programs/expand.nvim",
+    requires = { 'Sam-Programs/indent.nvim' } 
+    config = function() require("expand").setup {} end
+}
+```
+vim-plug
+```vim
+Plug 'Sam-Programs/expand.nvim'
+Plug 'Sam-Programs/indent.nvim'
+
+lua << EOF
+require("expand").setup {}
+EOF
+```
+## config
+adding custom pairs for other languages is simple 
+the syntax for the table is
+```
+'.*(.*)',{'{',          '}'},
+regex   ,{openning pair,closing pair}
+string    string        string
+function
+```
+note the pairs can be keys and the regex is vim `Pattern`
+the pattern is matched against the current line
+the plugin makes sure 'magic' is set while checking pairs
+
+alternatively you can use a function to evaluate wether to expand the pair or not
+which returns true if the pair should be chosen and false or nil(nothing)
+
+the table for languages is like a fallback table
+```lua
+-- this is checked first 
+{ 'function\\s*$',                     { '()', 'end' } },
+-- then this
+{ 'function',                     { '', 'end' } },
+```
+the final item in the list is used as a fallback if all other matches fail (doesn't get checked)
+
+and finally
+if there is no custom pair(s) for a filetype the plugin defaults to {}
+## default setup
+```lua
+require('expand').setup({
+   filetypes = {
+      lua = {
+         -- if we are expaning on an unnamed function might aswell add the pairs
+         { 'function\\s*$',                     { '()', 'end' } },
+         { 'function',                     { '', 'end' } },
+         { 'if',                           { ' then', 'end' } },
+         -- regex for a lua variable
+         { '^\\s*\\w\\+\\s*\\w*\\s*=\\s*', { '{', '}' } },
+         { 'this is not checked',                      { ' do', 'end' } },
+      },
+      sh = {
+         { 'elif',    { ' then', '' } },
+         { 'if',      { ' then', 'if' } },
+         { 'case',    { '', 'esac' } },
+         { '', { ' do', 'done' } },
+      },
+      bash = {
+         { 'elif',    { ' then', '' } },
+         { 'if',      { ' then', 'if' } },
+         { 'case',    { '', 'esac' } },
+         { '', { ' do', 'done' } },
+      },
+      zsh = {
+         { 'elif',    { ' then', '' } },
+         { 'if',      { ' then', 'if' } },
+         { 'case',    { '', 'esac' } },
+         { '', { ' do', 'done' } },
+      },
+      c = {
+         { '.*(.*)',  { '{', '}' } },
+         { '', { '{', '};' } },
+      },
+      cpp = {
+         { '.*(.*)',  { '{', '}' } },
+         { '', { '{', '};' } },
+      },
+   },
+   hotkey = '<C-Space>',
+})
+```
+## plans
+add a treesitter check to auto add comas in for lua tables inside lua tables
+allow the functions to return pairs
---
+++ lua/expand.lua
@@ -0,0 +1,111 @@
+local MATCH = 1
+local PAIRS = 2
+
+local default = {
+   filetypes = {
+      lua = {
+         -- if we are expaning on an unnamed function might aswell add the pairs
+         { 'function\\s*$',                     { '()', 'end' } },
+         { 'function',                     { '', 'end' } },
+         { 'if',                           { ' then', 'end' } },
+         -- regex for a lua variable
+         { '^\\s*\\w\\+\\s*\\w*\\s*=\\s*', { '{', '}' } },
+         { '',                      { ' do', 'end' } },
+      },
+      sh = {
+         { 'elif',    { ' then', '' } },
+         { 'if',      { ' then', 'if' } },
+         { 'case',    { '', 'esac' } },
+         { '', { ' do', 'done' } },
+      },
+      bash = {
+         { 'elif',    { ' then', '' } },
+         { 'if',      { ' then', 'if' } },
+         { 'case',    { '', 'esac' } },
+         { '', { ' do', 'done' } },
+      },
+      zsh = {
+         { 'elif',    { ' then', '' } },
+         { 'if',      { ' then', 'if' } },
+         { 'case',    { '', 'esac' } },
+         { '', { ' do', 'done' } },
+      },
+      c = {
+         { '.*(.*)',  { '{', '}' } },
+         { '', { '{', '};' } },
+      },
+      cpp = {
+         { '.*(.*)',  { '{', '}' } },
+         { '', { '{', '};' } },
+      },
+   },
+   hotkey = '<C-Space>',
+}
+
+local match =
+    function(str, pat)
+       if vim.fn.match(str, pat) ~= -1 then
+          return true
+       end
+       return false
+    end
+
+local function esc(str)
+   return vim.api.nvim_replace_termcodes(str, true, false, true)
+end
+
+local unpack = unpack or table.unpack
+local M = {}
+M.setup = function(opts)
+   local indent_installed, indent = pcall(require, 'indent')
+
+   if indent_installed == false then
+      print("indent.nvim is not installed can't setup expand.nvim")
+      return
+   end
+
+   M.config = vim.tbl_deep_extend("force",default, opts or {})
+   vim.keymap.set('i', M.config.hotkey, function()
+      local pair_open, pair_close = '{', '}'
+
+      local checks = M.config.filetypes[vim.o.filetype]
+      if checks then
+         OLD_magic = vim.o.magic
+         vim.o.magic = OLD_magic
+         local line = vim.api.nvim_get_current_line()
+         local success = false
+         for i = 1, #checks - 1, 1 do
+            if type(checks[i][MATCH]) == 'function' then
+               if checks[i][MATCH]() then
+                  pair_open, pair_close = unpack(checks[i][PAIRS])
+                  success = true
+                  break
+               end
+            end
+            if type(checks[i][MATCH]) == 'string' then
+               vim.o.magic = true
+               if match(line, checks[i][MATCH]) then
+                  pair_open, pair_close = unpack(checks[i][PAIRS])
+                  success = true
+                  break
+               end
+            else
+               print(checks[i][MATCH],"has an invalid match type (not a function or a string)")
+            end
+         end
+         if not success and #checks ~= 0 then
+            pair_open,pair_close = unpack(checks[#checks][PAIRS])
+         end
+      end
+      indent.enable_ctrl_f_formatting()
+      vim.api.nvim_feedkeys(
+         esc('<C-g>u' .. '<end>' ..
+            pair_open .. '<cr><cr>' ..
+            pair_close .. '<C-f><up><C-f>' ..
+            '<cmd>lua require(\'indent\').restore_user_configuration()' ..
+            ' vim.o.magic = OLD_magic<cr>'),
+         'n', false)
+   end)
+end
+
+return M
