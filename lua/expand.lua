ea18330a01e9ef69e951e49d60ab3f7539d78508
Sam-programs <None>
2023-10-30 13:43:43 +0300

    added options and cleaned the code a bit

--- lua/expand.lua
+++ lua/expand.lua
@@ -1,51 +1,45 @@
 local MATCH = 1
 local PAIRS = 2
-
+local OPT = 3
+local sh_rules = {
+   { 'elif',  { ' ;then', '' },  { endpair_new_line = false } },
+   { 'if',    { ' ;then', 'if' } },
+   { 'case',  { '', 'esac' } },
+   { 'while', { ' do', 'done' } },
+   { 'for',   { ' do', 'done' } },
+   { '',      { '{', '}' } },
+}
 local default = {
    filetypes = {
       python = {
-         { '',     { ':', '' } },
+         { '', { ':', '' }, { endpair_new_line = false } },
       },
       lua = {
          -- if we are expanding on an unnamed function might aswell add the pairs
-         { 'function\\s*$',                { '()', 'end' } },
-         { 'function',                     { '', 'end' } },
-         { 'if',                           { ' then', 'end' } },
+         { 'function[^(]*$',                { '()', 'end' },   { go_to_end = false } },
+         { 'function',                      { '', 'end' } },
+         { 'if',                            { ' then', 'end' } },
          -- regex for a lua variable
          { '^\\s*\\w\\+\\s*\\w*\\s*=\\s*$', { '{', '}' } },
-         { '',                             { ' do', 'end' } },
+         { '',                              { ' do', 'end' } },
       },
-      sh = {
-         { 'elif', { ' ;then', '' } },
-         { 'if',   { ' ;then', 'if' } },
-         { 'case', { '', 'esac' } },
-         { 'while',     { ' do', 'done' } },
-         { 'for',     { ' do', 'done' } },
-         { '',     { '{', '}' } },
-      },
-      bash = {
-         { 'elif', { ' ;then', '' } },
-         { 'if',   { ' ;then', 'if' } },
-         { 'case', { '', 'esac' } },
-         { 'while',     { ' do', 'done' } },
-         { 'for',     { ' do', 'done' } },
-         { '',     { '{', '}' } },
-      },
-      zsh = {
-         { 'elif', { ' then', '' } },
-         { 'if',   { ' then', 'if' } },
-         { 'case', { '', 'esac' } },
-         { 'while',     { ' do', 'done' } },
-         { 'for',     { ' do', 'done' } },
-         { '',     { '{', '}' } },
-      },
+      sh = sh_rules,
+      bash = sh_rules,
+      zsh = sh_rules,
       c = {
-         { '.*(.*)', { '{', '}' } },
-         { '',       { '{', '};' } },
+         { '.*(.*)',             { '{', '}' } },
+         { 'struct',             { '{', '};' } },
+         -- variable regex
+         { '[a-z1-9]\\s*=\\s*$', { '{', '};' } },
+         { '',                   { '', '' }, { do_nothing = true } },
       },
       cpp = {
-         { '.*(.*)', { '{', '}' } },
-         { '',       { '{', '};' } },
+         { '.*(.*)',             { '{', '}' } },
+         { 'class',              { '{', '};' } },
+         { '[a-z1-9]\\s*=\\s*$', { '{', '};' } },
+         { 'struct',             { '{', '};' } },
+         -- variable regex
+         { '',                   { '', '' }, { do_nothing = true } },
       },
    },
    hotkey = '<C-Space>',
@@ -63,6 +57,7 @@ local function esc(str)
    return vim.api.nvim_replace_termcodes(str, true, false, true)
 end
 
+---@diagnostic disable-next-line: deprecated
 local unpack = unpack or table.unpack
 local M = {}
 M.setup = function(opts)
@@ -77,45 +72,66 @@ M.setup = function(opts)
    vim.keymap.set('i', M.config.hotkey, function()
       local pair_open, pair_close = '{', '}'
 
-      local checks = M.config.filetypes[vim.o.filetype]
+      local rules = M.config.filetypes[vim.o.filetype]
       local old_magic = vim.o.magic
-      if checks then
+      local chosen_rule = nil
+      if rules then
          local line = vim.api.nvim_get_current_line()
-         local success = false
-         for i = 1, #checks - 1, 1 do
-            if type(checks[i][MATCH]) == 'function' then
-               local return_value,closing_pair = checks[i][MATCH]()
-               if type(return_value) == 'boolean' and return_value then
-                  pair_open, pair_close = unpack(checks[i][PAIRS])
-                  success = true
+         for _, rule in pairs(rules) do
+            if type(rule[MATCH]) == 'function' then
+               local return_value, closing_pair = rule[MATCH]()
+               if return_value == true then
+                  pair_open, pair_close = unpack(rule[PAIRS])
+                  chosen_rule = rule
                   break
                end
                if type(return_value) == 'string' then
-                  pair_open,pair_close = return_value,closing_pair
-                  success = true
+                  pair_open, pair_close = return_value, closing_pair
+                  chosen_rule = rule
                   break;
                end
-            elseif type(checks[i][MATCH]) == 'string' then
+            elseif type(rule[MATCH]) == 'string' then
                vim.o.magic = true
-               if match(line, checks[i][MATCH]) then
-                  pair_open, pair_close = unpack(checks[i][PAIRS])
-                  success = true
+               if match(line, rule[MATCH]) then
+                  pair_open, pair_close = unpack(rule[PAIRS])
+                  chosen_rule = rule
                   break
                end
-            else 
-               print(checks[i][MATCH], "has an invalid match type (not a function or a string)")
+            else
+               print(rule[MATCH], "has an invalid match type (not a function or a string)")
             end
          end
-         if not success and #checks ~= 0 then
-            pair_open, pair_close = unpack(checks[#checks][PAIRS])
+         if not chosen_rule and #rules ~= 0 then
+            pair_open, pair_close = unpack(rules[#rules][PAIRS])
+            chosen_rule = rules[#rules]
          end
       end
-      indent.enable_ctrl_f_formatting()
-      local keys = esc('<C-g>u<space><bs>' .. '<end>' ..
-         pair_open .. '<cr><cr>' ..
-         pair_close .. '<C-f><up><C-f><C-g>u' ..
+      local keys = ''
+      local end_key = '<end>'
+      local new_lines = '<cr><cr>'
+      local up_movement = '<up><C-f>'
+      if chosen_rule then
+         local opt = chosen_rule[OPT]
+         if opt then
+            if opt.do_nothing then
+               return
+            end
+            if opt.go_to_end == false then
+               end_key = ''
+            end
+            if opt.endpair_new_line == false then
+               new_lines = '<cr>'
+               up_movement = ''
+            end
+         end
+      end
+      keys = esc('<C-g>u<space><bs>' .. end_key ..
+         pair_open .. new_lines ..
+         pair_close .. '<C-f>' .. up_movement .. '<C-g>u' ..
          '<cmd>lua require(\'indent\').restore_user_configuration()' ..
          ' vim.o.magic = ' .. (old_magic and "true" or "false") .. 'OLD_magic<cr>')
+      indent.enable_ctrl_f_formatting()
+      ---@diagnostic disable-next-line: undefined-global
       if __EXPAND_IS_TESTING then
          -- tests loads every test into the typeahead bufer at once
          -- because i couldn't find a way to flush it
@@ -130,3 +146,4 @@ M.setup = function(opts)
 end
 
 return M
+
